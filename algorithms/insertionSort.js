af96a06a3d112ef76d0d9b9b4478a24227b850c9
vincentferraro <v.ferraro@live.fr>
2024-03-14 17:35:18 -0400

    BubbleSort

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,23 @@
+// Ascending
+
+function InserstionSort(arr){
+
+    for(let i = 0; i<arr.length;i++){
+        let numberToInsert=arr[i];
+        let j = i -1;
+        while(j>=0 && arr[j]>numberToInsert){
+            arr[j+1]= arr[j]
+            j=j-1;
+        }
+        arr[j+1]=numberToInsert;
+    }
+}
+
+// const arr1 = [ 8, 20, -2, 4, -6];
+// InserstionSort(arr1);
+// console.log(arr1);
+
+
+// Descending
+
+function InsertionSort2()
